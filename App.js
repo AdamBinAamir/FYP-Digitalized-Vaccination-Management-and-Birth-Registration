06e155f79ab3b1b1657d5ec9b555c054e6fc0335
Adam Bin Aamir <adam@Adams-MacBook-Pro.local>
2022-12-18 18:22:02 +0500

    second commit

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Start from './Screens/Start';
+import Login from './Screens/Login';
+import SignUp from './Screens/SignUp';
+import Dashboard from './Screens/Dashboard';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Start">
+        <Stack.Screen options={{ headerShown: false, title: 'Start' }}  name="Start" component={Start} />
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false, title: 'Login' }}/>
+        <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false, title: 'SignUp' }}/>
+        <Stack.Screen name="Dashboard" component={Dashboard} options={{ title: 'Dashboard' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -16,5 +29,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
---
+++ Screens/Feedback.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+import React, { useState } from 'react';
+
+export default function Family({navigation}) {
+
+  return (
+    
+    <View style={styles.container}>
+      <Text>Feedback</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
